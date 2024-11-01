e29a1327350420a6539c40e8e7bd01aad6c97319
Ersan Karimi <ersankarimii311@gmail.com>
2024-11-01 23:07:52 +0800

    feat: enable server-side rendering and add @nuxt/image module

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,12 +3,15 @@ export default defineNuxtConfig({
   compatibilityDate: "2024-11-01",
   devtools: { enabled: true },
 
+  // Enable this to perform server-side rendering in your app.
+  ssr: false,
+
   // Enable type-checking at build time: https://nuxt.com/docs/guide/concepts/typescript
   typescript: {
     typeCheck: true,
   },
 
-  modules: ["@nuxt/ui"],
+  modules: ["@nuxt/ui", "@nuxt/image"],
   colorMode: {
     preference: "system", // default value of $colorMode.preference
     fallback: "dark", // fallback value if not system preference found
