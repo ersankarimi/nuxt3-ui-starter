12810ec5f00ff3f84e02333f55b00c85addb4f65
Ersan Karimi <ersankarimii311@gmail.com>
2024-11-01 23:08:24 +0800

    feat: implement dynamic theme color and viewport settings in app.vue

--- app.vue
+++ app.vue
@@ -1,3 +1,22 @@
+<script setup lang="ts">
+const colorMode = useColorMode();
+
+const color = computed(() => colorMode.value === "dark" ? "#111827" : "#f1f5f9");
+
+useHead({
+  meta: [
+    { name: "viewport", content: "width=device-width, initial-scale=1" },
+    { key: "theme-color", name: "theme-color", content: color },
+  ],
+  link: [
+    { rel: "icon", type: "image/x-icon", href: "/favicon.ico" },
+  ],
+  htmlAttrs: {
+    lang: "en",
+  },
+});
+</script>
+
 <template>
   <div>
     <NuxtLayout>
@@ -5,3 +24,10 @@
     </NuxtLayout>
   </div>
 </template>
+
+<style lang="postcss">
+body {
+  @apply bg-slate-100 dark:bg-gray-900 text-slate-900 dark:text-gray-100;
+  transition: background-color 0.3s;
+}
+</style>
