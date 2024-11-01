c5875f1b039553d7cc45cbfeee1445b3b2c00e05
Ersan Karimi <ersankarimii311@gmail.com>
2024-11-02 03:19:04 +0800

    feat: enhance nuxt.config.ts with Nuxt Font and Image configurations

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -6,14 +6,40 @@ export default defineNuxtConfig({
   // Enable this to perform server-side rendering in your app.
   ssr: false,
 
-  // Enable type-checking at build time: https://nuxt.com/docs/guide/concepts/typescript
+  // Enable type-checking at build time (https://nuxt.com/docs/guide/concepts/typescript)
   typescript: {
     typeCheck: true,
   },
 
-  modules: ["@nuxt/ui", "@nuxt/image"],
+  // Nuxt Image Configuration (https://image.nuxt.com/advanced/static-images)
+  nitro: {
+    prerender: {
+      routes: [
+        "/_ipx/_/dots.svg",
+        "/_ipx/_/circle-blur.svg",
+      ],
+    },
+  },
+
+  modules: ["@nuxt/ui", "@nuxt/image", "@nuxt/fonts"],
   colorMode: {
     preference: "system", // default value of $colorMode.preference
     fallback: "dark", // fallback value if not system preference found
   },
+
+  // Nuxt Font Configuration (https://fonts.nuxt.com)
+  fonts: {
+    defaults: {
+      subsets: ["latin"],
+      weights: [400, 700],
+    },
+    families: [
+      {
+        name: "Manrope",
+        display: "swap",
+        provider: "google",
+      },
+    ],
+  },
+
 });
