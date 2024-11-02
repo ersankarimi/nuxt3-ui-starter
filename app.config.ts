f4d2d1c02c9e043508b5f5c49411d8e61f09af13
Ersan Karimi <ersankarimii311@gmail.com>
2024-11-02 22:26:48 +0800

    feat: add notification configuration for improved user feedback

--- app.config.ts
+++ app.config.ts
@@ -5,5 +5,17 @@ export default defineAppConfig({
     container: {
       constrained: "max-w-screen-xl",
     },
+    notifications: {
+      position: "top-0 bottom-auto",
+    },
+    notification: {
+      // eslint-disable-next-line ts/ban-ts-comment
+      // @ts-expect-error
+      default: {
+        color: "primary",
+        icon: null,
+        timeout: 5000,
+      },
+    },
   },
 });
