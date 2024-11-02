ffa2a1c32b33974c6861c8e3870f818bc32dec46
Ersan Karimi <ersankarimii311@gmail.com>
2024-11-02 17:25:31 +0800

    feat: add tour page with welcome message and navigation prompt

---
+++ pages/tour/index.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="mt-8">
+    <UCard class="text-center flex-1">
+      <h1 class="text-5xl md:text-6xl lg:text-7xl font-bold text-green-500 flex flex-col">
+        <span>
+          Hi there!
+        </span>
+        <span>
+          Welcome to the tour page.
+        </span>
+      </h1>
+
+      <p class="text-xl lg:text-2xl mt-2">
+        Select a page from the navigation to get started.
+      </p>
+    </UCard>
+  </div>
+</template>
